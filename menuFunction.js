4dc80bcab67bdd03e616adedaa7ed4512d3b94f8
Dillan Veranga <68349497+verangad@users.noreply.github.com>
2021-03-29 18:18:29 -0500

    update29/3/20

---
+++ menuFunction.js
@@ -0,0 +1,24 @@
+function menuFunction(input) {
+
+    var homeVar = document.getElementsByClassName("homeContent");
+    var resumeVar = document.getElementsByClassName("resumeContent");
+   
+    if(input == 0){
+      homeVar[0].style.color = "blue";
+      resumeVar[0].style.display = "none";
+      resumeVar[0].style.opacity = "0";
+      
+    }
+    else if(input == 1){
+      location.href="resume.html";
+    }
+    else if(input == 2){
+
+
+    }
+    else if(input == 2){
+
+
+    }
+
+}
