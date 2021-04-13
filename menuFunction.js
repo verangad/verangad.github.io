6569d3222e28ef00f965d9617c3cdbb4c3218542
Dillan Veranga <68349497+verangad@users.noreply.github.com>
2021-04-13 15:07:24 -0500

    update13/4/21

--- menuFunction.js
+++ menuFunction.js
@@ -1,25 +1,36 @@
-function menuFunction(input) {
-
-    var homeVar = document.getElementsByClassName("homeContent");
-    var resumeVar = document.getElementsByClassName("resumeContent");
-   
-    if(input == 0){
-      homeVar[0].style.color = "blue";
-      resumeVar[0].style.display = "none";
-      resumeVar[0].style.opacity = "0";
-      
-    }
-    else if(input == 1){
-      location.href="resume.html";
-      
-    }
-    else if(input == 2){
 
 
-    }
-    else if(input == 2){
+function projectPages(tab, clickedButton) {
 
+  var i;
+  var numTabs;
+  var tabButtons;
+  
+  numTabs = document.getElementsByClassName("project-content");
+  tabButtons = document.getElementsByClassName("project-button");
+  
+  for (i = 0; i < numTabs.length; i++) { //hide all content in all tabs
 
-    }
+    numTabs[i].style.display = "none";
 
+  }
+
+  for (i = 0; i < tabButtons.length; i++) { //reset color of buttons
+
+    tabButtons[i].style.backgroundColor = "";
+
+  }
+  
+  //show the clicked tab's content
+  document.getElementById(tab).style.display = "block";
+  //set clicked button color
+  clickedButton.style.backgroundColor = "#777";
+  
 }
+
+//default pages
+document.getElementById("default").click();
+
+
+
+
---
+++ resumeFunction.js
@@ -0,0 +1,31 @@
+function resumePages(tab, clickedButton) {
+
+    var i;
+    var docTabs;
+    var docButtons;
+    
+    docTabs = document.getElementsByClassName("resume-content");
+    docButtons = document.getElementsByClassName("resume-button");
+    
+    for (i = 0; i < docTabs.length; i++) { //hide all content in all tabs
+  
+      docTabs[i].style.display = "none";
+  
+    }
+  
+    for (i = 0; i < docButtons.length; i++) { //reset color of buttons
+  
+      docButtons[i].style.backgroundColor = "";
+  
+    }
+    
+    //show the clicked tab's content
+    document.getElementById(tab).style.display = "block";
+    //set clicked button color
+    clickedButton.style.backgroundColor = "#777";
+    
+}
+  
+  
+document.getElementById("default-resume").click();
+  
