e503bff9c29893438c56ee35124bbe6e1d10d76d
Dillan Veranga <68349497+verangad@users.noreply.github.com>
2021-05-08 15:18:43 -0500

    update8/5/21

--- menuFunction.js
+++ menuFunction.js
@@ -1,18 +1,32 @@
-
+let currentSlide = 0;
 
 function projectPages(tab, clickedButton) {
 
-  var i;
-  var numTabs;
-  var tabButtons;
+  let i;
+  let numTabs;
+  let tabButtons;
+  let slideshow;
+  let numChild;
+
   
   numTabs = document.getElementsByClassName("project-content");
   tabButtons = document.getElementsByClassName("project-button");
+  slideshow = document.getElementsByClassName("type-lang");
   
   for (i = 0; i < numTabs.length; i++) { //hide all content in all tabs
 
     
     numTabs[i].style.display = "none";
+
+  //show the clicked tab's content
+    if(numTabs[i].id === tab){
+
+      document.getElementById(tab).style.display = "block";
+      numChild = slideshow[i].childElementCount;
+
+      startSlide(childElementCount);
+       
+    }
 
   }
 
@@ -22,11 +36,31 @@ function projectPages(tab, clickedButton) {
 
   }
   
-  //show the clicked tab's content
-  document.getElementById(tab).style.display = "block";
+
+  
   //set clicked button color
   clickedButton.style.color = "yellow";
   
+}
+
+
+function startSlide(size){
+
+  currentSlide = 0;
+  setTimeout(function() { changeSlide(size); }, 5000);
+
+}
+
+function changeSlide(size){
+
+  window.alert(size);
+
+
+  currentSlide++;
+  if(currentSlide === size){
+
+    currentSlide = 0;
+  }
 }
 
 //default pages
